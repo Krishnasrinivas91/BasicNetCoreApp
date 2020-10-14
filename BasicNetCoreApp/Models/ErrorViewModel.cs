6dc59f2f46dcd516cfd41df5f57415941f935ddb
krishnasrinivas91 <krishnasrinivas91@gmail.com>
2020-10-14 16:26:54 +0530

    Add project files.

---
+++ BasicNetCoreApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BasicNetCoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
